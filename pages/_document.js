b0511579d51c00ee8cb25f28e5bb832191971836
Dr-Sushant <dr.sushant.tapase@gmail.com>
2025-12-17 22:00:14 +0530

    Add canonical URL and Google site verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Search Console verification */}
         <meta
           name="google-site-verification"
           content="EOBVgmoShLRMlM5vHr0TvOH_W6mJfxtvKoUHESK6hFU"
         />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://steth2tech.com" />
       </Head>
       <body className="antialiased">
         <Main />
