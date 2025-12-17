3b651450bec85a0ae584252d46e4dfbb49ffb685
Dr-Sushant <dr.sushant.tapase@gmail.com>
2025-12-17 21:47:07 +0530

    Fix Google Search Console verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="EOBVgmoShLRMlM5vHr0TvOH_W6mJfxtvKoUHESK6hFU"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
@@ -11,3 +16,5 @@ export default function Document() {
     </Html>
   );
 }
+
+
