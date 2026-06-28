bdbe4496b52c9ac6d9e29be26860488ad3243a81
Dr-Sushant <dr.sushant.tapase@gmail.com>
2026-06-29 00:24:00 +0530

    Add Google Analytics 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GoogleAnalytics gaId="G-22Y0T2WNZB" />
+
       <Component {...pageProps} />
+
       <Analytics />
     </>
   );
